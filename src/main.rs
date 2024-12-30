c1442ee9a8ca43e94f11433dba3a5564755935e9
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-29 21:26:03 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ fn main() {
         stdin.read_line(&mut input).expect("stdin to read input");
 
         let Some(command) = input.trim_end().split_once(" ") else {
+            println!("{}: command not found", input.trim());
             continue;
         };
 
         match command {
-            ("echo", str) => println!("echo {}", str),
+            ("echo", str) => println!("{}", str),
             ("exit", _) => std::process::exit(0),
             (command, _) => println!("{}: command not found", command),
         }
