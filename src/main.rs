d9e597542192cf1d2eb51f58eee2fe807410ec59
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-29 23:20:20 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,84 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
-use std::{
-    env::{self},
-    fs,
-    path::{Path, PathBuf},
-};
+use std::io::Write;
+use std::{env, io::BufRead, process::Command};
 
-enum Command {
-    Echo,
-    Exit,
-    Type,
-}
+fn handle_command(args: Vec<&str>) {
+    let mut args = args.iter();
+    let Some(command) = args.next() else {
+        return;
+    };
 
-impl From<&str> for Command {
-    fn from(command_str: &str) -> Self {
-        match command_str {
-            "echo" => Self::Echo,
-            "exit" => Self::Exit,
-            "type" => Self::Type,
-            _ => panic!("command does not exist"),
+    let rest: Vec<&str> = args.copied().collect();
+
+    let rest = rest.join(" ");
+    let rest: &str = rest.as_ref();
+
+    match *command {
+        "echo" => {
+            println!("{}", rest);
         }
-    }
-}
+        "exit" => std::process::exit(rest.parse::<i32>().expect("code should be a valid number")),
+        "type" => {
+            if matches!(rest, "echo" | "exit" | "type") {
+                println!("{} is a shell builtin", rest);
+            } else {
+                let paths = std::env::var("PATH").expect("PATH should be set");
 
-impl Command {
-    fn handle_command(self, rest: &str) {
-        let rest = rest.trim();
-
-        match self {
-            Command::Echo => {
-                println!("{}", rest);
-            }
-            Command::Exit => {
-                std::process::exit(rest.parse::<i32>().expect("code should be a valid number"))
-            }
-            Command::Type => {
-                if matches!(rest, "echo" | "exit" | "type") {
-                    println!("{} is a shell builtin", rest);
-                } else {
-                    let paths = env::var("PATH").expect("PATH should be set");
-
-                    for path in paths.split(":") {
-                        let mut path = PathBuf::from(path);
-                        path.push(rest);
-
-                        if path.exists() {
-                            println!(
-                                "{} is {}",
-                                rest,
-                                path.to_str().expect("Path should be shown as string")
-                            );
-                            return;
-                        }
+                if let Some(path) = env::split_paths(&paths).find_map(|path| {
+                    let path = path.join(rest);
+                    if path.is_file() {
+                        return Some(path);
                     }
-
+                    None
+                }) {
+                    println!(
+                        "{} is {}",
+                        rest,
+                        path.into_os_string().into_string().unwrap()
+                    );
+                } else {
                     println!("{}: not found", rest);
                 }
+            }
+        }
+        _ => {
+            let paths = std::env::var("PATH").expect("PATH should be set");
+
+            if let Some(path) = env::split_paths(&paths).find_map(|path| {
+                let path = path.join(command);
+                if path.is_file() {
+                    return Some(path);
+                }
+                None
+            }) {
+                Command::new(path.into_os_string().into_string().unwrap())
+                    .arg(rest)
+                    .status()
+                    .expect("failed to execute process");
+            } else {
+                println!("{}: command not found", command);
             }
         }
     }
 }
 
 fn main() {
+    let mut stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout().lock();
+
     loop {
         print!("$ ");
-        io::stdout().flush().unwrap();
-
-        let stdin = io::stdin();
+        stdout.flush().unwrap();
 
         let mut input = String::new();
         stdin.read_line(&mut input).expect("stdin to read input");
 
-        let Some((command, rest)) = input.trim_end().split_once(" ") else {
-            println!("{}: command not found", input.trim());
+        let args: Vec<&str> = input.trim_end().split(" ").collect();
+
+        if args.is_empty() {
             continue;
-        };
+        }
 
-        let command = Command::from(command);
-
-        command.handle_command(rest);
+        handle_command(args);
     }
 }
