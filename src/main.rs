5b48483c22fbcad1eccd0d9bc5f2702d7dc5cfe1
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2024-12-29 21:39:39 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command {
+    Echo,
+    Exit,
+    Type,
+}
+
+impl From<&str> for Command {
+    fn from(command_str: &str) -> Self {
+        match command_str {
+            "echo" => Self::Echo,
+            "exit" => Self::Exit,
+            "type" => Self::Type,
+            _ => panic!("command does not exist"),
+        }
+    }
+}
+
+impl Command {
+    fn handle_command(self, rest: &str) {
+        let rest = rest.trim();
+
+        match self {
+            Command::Echo => {
+                println!("{}", rest);
+            }
+            Command::Exit => {
+                std::process::exit(rest.parse::<i32>().expect("code should be a valid number"))
+            }
+            Command::Type => {
+                if matches!(rest, "echo" | "exit" | "type") {
+                    println!("{} is a shell builtin", rest);
+                } else {
+                    println!("{}: not found", rest);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -11,15 +50,13 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).expect("stdin to read input");
 
-        let Some(command) = input.trim_end().split_once(" ") else {
+        let Some((command, rest)) = input.trim_end().split_once(" ") else {
             println!("{}: command not found", input.trim());
             continue;
         };
 
-        match command {
-            ("echo", str) => println!("{}", str),
-            ("exit", _) => std::process::exit(0),
-            (command, _) => println!("{}: command not found", command),
-        }
+        let command = Command::from(command);
+
+        command.handle_command(rest);
     }
 }
